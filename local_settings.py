809e7246b6fc25f1ac07f22bacb483ea04d4c33a
Risfio <risfio1982@gmail.com>
2014-08-25 18:20:14 +0000

    Adding some comentary in  local_settings.py

--- local_settings.py
+++ local_settings.py
@@ -3,6 +3,12 @@
 import os
 from django.contrib.staticfiles import storage
 
+# This pathes will be used in templates.
+# We will not use {% load static from staticfiles %}
+# Just use {{ styles }} and the same.
+# This variables will used in views code in response context.
+# I do it - cause in gulp projects i can use just {{ [variable name] }}
+# constructions.
 static = storage.ConfiguredStorage()
 styles = os.path.join(static.location, 'css')
 scripts = os.path.join(static.location, 'js')
